b473abcce76a7f9dc006a02b208317522822aafc
ravikiran.padki <ravikiran.padki@gmail.com>
2025-02-18 23:08:29 +1100

    updated test data

--- src/tests/login.spec.ts
+++ src/tests/login.spec.ts
@@ -5,12 +5,15 @@ import data from './../data/data.json';
 let commonUtils: CommonUtils
 let username: string
 let password: string
+let incorrect_username: string
 
 test.beforeEach(async ({ navigationPageMethods, page }) => {
     await navigationPageMethods.goToLoginURL();
     commonUtils = new CommonUtils(page);
     username = data.users[0].username;
     password = data.users[0].password;
+    incorrect_username = data.users[1].incorrect_username
+
 });
 
 test('Verify the login page components', async ({ loginPageMethods, page }) => {
@@ -21,8 +24,8 @@ test('Verify the login page components', async ({ loginPageMethods, page }) => {
     expect(await loginPageMethods.getPassword()).toHaveAttribute("placeholder");
 });
 
-test('Incorrect login credentials throws error', async ({ loginPageMethods, page }) => {
-    await loginPageMethods.login(commonUtils.encryptCredentials(username), commonUtils.encryptCredentials(password));
+test.only('Incorrect login credentials throws error', async ({ loginPageMethods, page }) => {
+    await loginPageMethods.login(commonUtils.encryptCredentials(incorrect_username), commonUtils.encryptCredentials(password));
     expect(await loginPageMethods.getErrorMessage()).toBe("Epic sadface: Username and password do not match any user in this service");
 });
 
